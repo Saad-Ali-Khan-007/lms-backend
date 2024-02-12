4815470609fa6f55869e5d558c6b6cd0b96f825b
Saad Ali Khan <saadalikhan042@gmail.com>
2024-02-12 14:01:32 +0500

    login view with csfr exempt

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('teachers/',views.TeacherList.as_view()),
     path('teachers/<int:pk>/', views.TeacherDetail.as_view()),
+    path('teachers/login/',views.teacher_login)
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from . import models
 from rest_framework import permissions
 from .serializers import TeacherSerializer 
 from rest_framework import generics
+from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 class TeacherList(generics.ListCreateAPIView):
@@ -17,3 +19,12 @@ class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
     # permission_classes = [permissions.IsAuthenticated]
+@csrf_exempt
+def teacher_login(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    teacherData = models.Teacher.objects.get(email=email,password=password)
+    if teacherData:
+        return JsonResponse({"bool":True})  
+    else:
+        return JsonResponse({"bool":False})
